dab143adbae4269e0fe7f4ac94be3bf96e0beadb
milan bista <milanbista@gmail.com>
2022-02-07 22:21:45 -0600

    added twosum

---
+++ src/TwoSum.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+		
+		//find out if the given number is obtained by adding two numbers from the given array
+		//example [1,4,7,9] sum = 10
+		int[] array = {1,2,7,9};
+		int sum = 9;
+		System.out.println(twoSum(array, sum));
+		
+	}
+	
+	public static boolean twoSum(int array[], int sum) {
+		Map<Integer, Integer> lookup = new HashMap<Integer, Integer>();
+		
+		for(int num: array) {
+			if(lookup.containsKey(num))
+				return true;
+			lookup.put(sum-num, 1);
+		}
+		
+		return false;
+	}
+}
